416d443f1ee11399589da40a1090afa9c135cd27
baloyd <danielbaloy@gmail.com>
2021-05-14 11:47:07 -0400

    organized html files back into public folder

--- Assets/js/server.js
+++ Assets/js/server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const path = require("path")
 const fs= require("fs");
 
+
 // Sets up the Express App
 
 const app = express();
@@ -14,10 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Sets default path to the contents of the index.html file.
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 //When /notes is navigated to, the contents of the notes.html file will be displayed.
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
 
