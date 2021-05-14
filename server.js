236675f27eacb72822e0b8862e1d6729fce67b2d
baloyd <danielbaloy@gmail.com>
2021-05-14 12:57:16 -0400

    adjusted file structure to have important files at the root

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 9000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 //Sets default path to the contents of the index.html file.
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
@@ -20,7 +21,14 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 //When /notes is navigated to, the contents of the notes.html file will be displayed.
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
+app.get('/api/notes', function(req, res) {
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) {
+      throw err;}
+      dbData = JSON.parse(data);
+      res.send(dbData);
+    });
+  });
 
 // Starts the server to begin listening
 
